a9c9fa4f49fcc1f73601a0f4d72acee323967ce8
Renoh47 <renoh47@gmail.com>
2017-10-25 00:23:39 -0700

    Python 3 updates, added missing image

--- pychal/pychal11.py
+++ pychal/pychal11.py
@@ -8,12 +8,10 @@ row = 0
 col = 0
 
 for col in range(0, image.size[0]):
-	print col
 	for row in range(0, image.size[1]):
-		print row
 		if(row%2 == 0):
-			one.putpixel((col/2, row/2), image.getpixel((col, row)))
+			one.putpixel((col//2, row//2), image.getpixel((col, row)))
 		else:
-			two.putpixel((col/2, row/2), image.getpixel((col, row)))
-one.save("test1", "JPEG")
-two.save("test2", "JPEG")
+			two.putpixel((col//2, row//2), image.getpixel((col, row)))
+one.save("test1.jpg", "JPEG")
+two.save("test2.jpg", "JPEG")
